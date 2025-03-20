2061ce6cffc7cf147ba189f17a2ec91080705d7c
Carles Aguilera <carlesaguilera@MacBook-Air-de-Carles.local>
2025-03-20 09:21:19 +0100

    arreglat

--- app/views.py
+++ app/views.py
@@ -339,7 +339,6 @@ def get_ruta(request, pk):
 @permission_classes([AllowAny])
 def create_ruta(request):
     data = {
-        'etiqueta': request.data.get('etiqueta'),
         'descripcio': request.data.get('descripcio'),
         'nom': request.data.get('nom'),
         'dist_km': request.data.get('dist_km')

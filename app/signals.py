8285f53a54c91db519aa05f8d96378224a39bcf6
unknown <aguilerapilo@gmail.com>
2025-05-05 11:03:11 +0200

    afegit detalls per al correcte funcionament

--- app/signals.py
+++ app/signals.py
@@ -34,6 +34,10 @@ def notificar_cambio_modelo(sender, instance, created=None, **kwargs):
     if os.environ.get("DISABLE_SIGNALS", "").lower() == "true":
         return
 
+    # Ignorar señales de creación, actualización o eliminación de Ruta y Presencia
+    if sender.__name__ in ["Ruta", "Presencia"]:
+        return
+
     evento = (
         "creado" if created else "modificado" if created is not None else "eliminado"
     )
@@ -74,19 +78,23 @@ def notificar_cambio_modelo(sender, instance, created=None, **kwargs):
 
     print(f"SEÑAL DISPARADA: {sender.__name__} - {obj_id} - created={created}")
 
-    async_to_sync(get_channel_layer().group_send)(
-        "model_updates",
-        {
-            "type": "send_model_update",
-            "data": {
-                "tipo": evento,
-                "modelo": sender.__name__,
-                "id": obj_id,
-                "datos": datos_dict,
-                "timestamp": timestamp,
+    try:
+        async_to_sync(get_channel_layer().group_send)(
+            "model_updates",
+            {
+                "type": "send_model_update",
+                "data": {
+                    "tipo": evento,
+                    "modelo": sender.__name__,
+                    "id": obj_id,
+                    "datos": datos_dict,
+                    "timestamp": timestamp,
+                },
             },
-        },
-    )
+        )
+    except Exception as e:
+        print(f"Redis no disponible o canal lleno, mensaje ignorado: {e}")
+        pass
 
 
 # Registrar señales para todos los modelos

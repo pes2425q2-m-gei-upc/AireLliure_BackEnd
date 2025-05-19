231a0ab32573c9dad37de863c1342d5df85b690e
carles-aguilera-pilo <aguilerapilo@gmail.com>
2025-05-19 11:49:47 +0200

    Locust i les probes de carrega

--- app/tests/locust.py
+++ app/tests/locust.py
@@ -1,6 +1,8 @@
-import time
-from locust import HttpUser, task, between
 import random
+import time
+
+from locust import HttpUser, between, task
+
 
 class WebsiteUser(HttpUser):
     host = "https://airelliure-backend.onrender.com"
@@ -9,10 +11,13 @@ class WebsiteUser(HttpUser):
     @task
     def on_start(self):
         try:
-            response = self.client.post("/login/", json={
-                "correu": "aguilera@example.com",
-                "password": "ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f"
-            })
+            response = self.client.post(
+                "/login/",
+                json={
+                    "correu": "aguilera@example.com",
+                    "password": "ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f",
+                },
+            )
             if response.status_code == 200:
                 print("Login exitoso")
             else:
@@ -73,38 +78,45 @@ class WebsiteUser(HttpUser):
         response = self.client.get("/usuaris/aguilera@example.com/")
         if response.status_code != 200:
             print(f"Error en usuari: {response.status_code}")
+
     @task
     def get_amdins(self):
         response = self.client.get("/admins/")
         if response.status_code != 200:
             print(f"Error en amdins: {response.status_code}")
+
     @task
     def get_amdins_by_pk(self):
         response = self.client.get("/admins/aguilera@example.com/")
         if response.status_code != 200:
-            print(f"Error en amdins: {response.status_code}")    
+            print(f"Error en amdins: {response.status_code}")
+
     @task
     def get_all_bloqueigs(self):
         response = self.client.get("/bloqueigs/")
         if response.status_code != 200:
-            print(f"Error en bloqueigs: {response.status_code}")    
+            print(f"Error en bloqueigs: {response.status_code}")
+
     @task
     def get_all_amistats(self):
         response = self.client.get("/amistats/")
         if response.status_code != 200:
-            print(f"Error en amistats: {response.status_code}")      
+            print(f"Error en amistats: {response.status_code}")
+
     @task
     def get_all_rutes(self):
         response = self.client.get("/rutas/")
         if response.status_code != 200:
-            print(f"Error en rutas: {response.status_code}")    
+            print(f"Error en rutas: {response.status_code}")
+
     @task
     def get_ruta_by_pk(self):
         response = self.client.get("/rutas/615851413/")
         if response.status_code != 200:
-            print(f"Error en ruta: {response.status_code}")    
-    @task 
+            print(f"Error en ruta: {response.status_code}")
+
+    @task
     def get_info_ruta(self):
-        response = self.client.get('/rutas/615851413/info/')
+        response = self.client.get("/rutas/615851413/info/")
         if response.status_code != 200:
             print(f"Error en ruta: {response.status_code}")
